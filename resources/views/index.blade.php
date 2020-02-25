e1d8134b734df9352f6e5398282c6b0004a227f2
Sonika Baniya <sonika.baniya.sb@gmail.com>
2020-02-25 15:20:34 +0545

    Front page minor changes

---
+++ resources/views/index.blade.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.12.1, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="{{ asset('assets/images/swastha-nari-svg-logo.svg')}}" type="image/x-icon">
+  <meta name="description" content="">
+  
+  <title>Home</title>
+  <link rel="stylesheet" href="{{ asset('assets/web/assets/mobirise-icons/mobirise-icons.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/facebook-plugin/style.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap-grid.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap-reboot.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/dropdown/css/style.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/tether/tether.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/socicon/css/styles.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/theme/css/style.css')}}">
+  <link rel="stylesheet" href="{{ asset('assets/gallery/style.css')}}">
+  <link rel="preload" as="style" href="{{ asset('assets/mobirise/css/mbr-additional.css')}}"><link rel="stylesheet" href="{{ asset('assets/mobirise/css/mbr-additional.css')}}" type="text/css">
+  
+  
+  
+</head>
+<body>
+  <section class="menu cid-qTkzRZLJNu" once="menu" id="menu1-0">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm bg-color transparent">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="https://swasthanaari.com">
+                         <img src="{{ asset('assets/images/swastha-nari-svg-logo.svg')}}" alt="Swastha Naari" title="" style="height: 3.8rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-primary display-4" href="https://swasthanaari.com">स्वस्थ नारी</a></span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item">
+                    <a class="nav-link link text-primary display-4" href="https://swasthanaari.com"><span class="mbri-home mbr-iconfont mbr-iconfont-btn"></span>
+                        
+                        Home</a>
+                </li></ul>
+            <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-primary display-4" href="https://swasthanaari.com/login"><span class="mbri-login mbr-iconfont mbr-iconfont-btn"></span>
+                    
+                    Login/Register</a></div>
+        </div>
+    </nav>
+</section>
+
+<section class="engine"><a href="https://mobirise.info/q">responsive site templates</a></section><section class="cid-qTkA127IK8 mbr-fullscreen mbr-parallax-background" id="header2-1">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.6; background-color: rgb(91, 104, 107);"></div>
+
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">स्वस्थ नारी — हामी सबैको जिम्मेवारी</h1>
+                
+                <p class="mbr-text pb-3 mbr-fonts-style display-5">
+                    Swastha Naari is an initiative of four, then senior undergraduate, tech students to focuses on the importance of data in aspect to women’s reproductive health. Talking about our solution, Swastha Naari is an android as well as a web application that helps to collect data in health posts and hospitals in the context of Nepal which is sadly paper-based till today’s date.
+                </p>
+                <div class="mbr-section-btn"><a class="btn btn-md btn-primary display-4" href="https://swasthnaari.com/login">GET STARTED</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+        <a href="#next">
+            <i class="mbri-down mbr-iconfont"></i>
+        </a>
+    </div>
+</section>
+
+<section class="mbr-section article content10 cid-rRuWGTifWP" id="content10-e">
+    
+     
+
+    <div class="container">
+        <div class="inner-container" style="width: 66%;">
+            <hr class="line" style="width: 25%;">
+            <div class="section-text align-center mbr-white mbr-fonts-style display-5">Swastha Naari conducted its first project launch conduct i in the rural part of Nepal, Jyamire village on 3rd August 2019 which is situated at the Hetauda district.</div>
+            <hr class="line" style="width: 25%;">
+        </div>
+    </div>
+</section>
+
+<section class="mbr-gallery mbr-slider-carousel cid-rRuWJhaxf6" id="gallery4-f">
+
+    
+
+    <div class="container">
+        <div><!-- Gallery --><div class="gallery-row"><div class="mbr-gallery-layout-default"><div><div class="row-first row justify-content-center m-0"><div class="mbr-gallery-item col-lg-4 col-md-4 col-sm-12 p-2" data-video-url="false"><div href="#lb-gallery4-f" data-slide-to="0" data-toggle="modal"><img src="{{ asset('assets/images/dsc0945-2000x1325-800x530.jpg')}}" alt="Youngest Attendee" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Youngest Attendee</span></div></div><div class="mbr-gallery-item col-lg-4 col-md-4 col-sm-12 p-2" data-video-url="false"><div href="#lb-gallery4-f" data-slide-to="1" data-toggle="modal"><img src="{{ asset('assets/images/dsc1101-2000x1325-800x530.jpg')}}" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Donating Books</span></div></div><div class="mbr-gallery-item col-lg-4 col-md-4 col-sm-12 p-2" data-video-url="false"><div href="#lb-gallery4-f" data-slide-to="2" data-toggle="modal"><img src="{{ asset('assets/images/dsc1104-2000x1325-800x530.jpg')}}" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Donating Books</span></div></div></div><div class="row-second row justify-content-center m-0"><div class="mbr-gallery-item col-lg-3 col-md-6 col-sm-12 p-2" data-video-url="false"><div href="#lb-gallery4-f" data-slide-to="3" data-toggle="modal"><img src="{{ asset('assets/images/dsc0037-2000x1325-800x530.jpg')}}" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Whole team in Jyamire village</span></div></div><div class="mbr-gallery-item col-lg-3 col-md-6 col-sm-12 p-2" data-video-url="false"><div href="#lb-gallery4-f" data-slide-to="4" data-toggle="modal"><img src="{{ asset('assets/images/dsc0007-2000x1325-800x530.jpg')}}" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">Kids who attended program</span></div></div><div class="mbr-gallery-item col-lg-3 col-md-6 col-sm-12 p-2" data-video-url="false"><div href="#lb-gallery4-f" data-slide-to="5" data-toggle="modal"><img src="{{ asset('assets/images/dsc1071-2000x1325-800x530.jpg')}}" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">The whole audience</span></div></div><div class="mbr-gallery-item col-lg-3 col-md-6 col-sm-12 p-2" data-video-url="false"><div href="#lb-gallery4-f" data-slide-to="6" data-toggle="modal"><img src="{{ asset('assets/images/dsc1013-2000x1325-800x530.jpg')}}" alt="" title=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">In between program</span></div></div></div></div><div class="clearfix"></div></div></div><!-- Lightbox --><div data-app-prevent-settings="" class="mbr-slider modal fade carousel slide" tabindex="-1" data-keyboard="true" data-interval="false" id="lb-gallery4-f"><div class="modal-dialog"><div class="modal-content"><div class="modal-body"><div class="carousel-inner"><div class="carousel-item active"><img src="{{ asset('assets/images/-dsc0945-2000x1325.jpg')}}" alt="Youngest Attendee" title=""></div><div class="carousel-item"><img src="{{ asset('assets/images/-dsc1101-2000x1325.jpg')}}" alt="" title=""></div><div class="carousel-item"><img src="{{ asset('assets/images/-dsc1104-2000x1325.jpg')}}" alt="" title=""></div><div class="carousel-item"><img src="{{ asset('assets/images/-dsc0037-2000x1325.jpg')}}" alt="" title=""></div><div class="carousel-item"><img src="{{ asset('assets/images/-dsc0007-2000x1325.jpg')}}" alt="" title=""></div><div class="carousel-item"><img src="{{ asset('assets/images/-dsc1071-2000x1325.jpg')}}" alt="" title=""></div><div class="carousel-item"><img src="{{ asset('assets/images/-dsc1013-2000x1325.jpg')}}" alt="" title=""></div><div class="carousel-item"><img src="{{ asset('assets/images/background8.jpg')}}" alt="" title=""></div></div><a class="carousel-control carousel-control-prev" role="button" data-slide="prev" href="#lb-gallery4-f"><span class="mbri-left mbr-iconfont" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control carousel-control-next" role="button" data-slide="next" href="#lb-gallery4-f"><span class="mbri-right mbr-iconfont" aria-hidden="true"></span><span class="sr-only">Next</span></a><a class="close" href="#" role="button" data-dismiss="modal"><span class="sr-only">Close</span></a></div></div></div></div></div>
+    </div>
+
+</section>
+
+<section class="mbr-section form1 cid-rRuUOH2143" id="form1-9">
+
+    
+
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="title col-12 col-lg-8">
+                <h2 class="mbr-section-title align-center pb-3 mbr-fonts-style display-2">
+                    CONTACT FORM
+                </h2>
+                <h3 class="mbr-section-subtitle align-center mbr-light pb-3 mbr-fonts-style display-5">Contact us for any queries or collaboration for digital women data campaigns and we should get back to you shortly!</h3>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="media-container-column col-lg-8" data-form-type="formoid">
+                <!---Formbuilder Form--->
+                <form action="https://mobirise.com/" method="POST" class="mbr-form form-with-styler" data-form-title="Mobirise Form"><input type="hidden" name="email" data-form-email="true" value="sBOq9CV69FfvzIWYtyx0e8KB6nbRaKo5lgnHjrH/TCZEg/aTHy0rvKHAmg1pZHPHAA1O7dDUKZaCPu9rP9aB6qYol+VV5CxpdQ45pxFXnJKHDFMNpNymtevDAZvpq30J">
+                    <div class="row">
+                        <div hidden="hidden" data-form-alert="" class="alert alert-success col-12">Thanks for filling out the form!</div>
+                        <div hidden="hidden" data-form-alert-danger="" class="alert alert-danger col-12">
+                        </div>
+                    </div>
+                    <div class="dragArea row">
+                        <div class="col-md-4  form-group" data-for="name">
+                            <label for="name-form1-9" class="form-control-label mbr-fonts-style display-7">Name</label>
+                            <input type="text" name="name" data-form-field="Name" required="required" class="form-control display-7" id="name-form1-9">
+                        </div>
+                        <div class="col-md-4  form-group" data-for="email">
+                            <label for="email-form1-9" class="form-control-label mbr-fonts-style display-7">Email</label>
+                            <input type="email" name="email" data-form-field="Email" required="required" class="form-control display-7" id="email-form1-9">
+                        </div>
+                        <div data-for="phone" class="col-md-4  form-group">
+                            <label for="phone-form1-9" class="form-control-label mbr-fonts-style display-7">Phone</label>
+                            <input type="tel" name="phone" data-form-field="Phone" class="form-control display-7" id="phone-form1-9">
+                        </div>
+                        <div data-for="message" class="col-md-12 form-group">
+                            <label for="message-form1-9" class="form-control-label mbr-fonts-style display-7">Message</label>
+                            <textarea name="message" data-form-field="Message" class="form-control display-7" id="message-form1-9"></textarea>
+                        </div>
+                        <div class="col-md-12 input-group-btn align-center"><button type="submit" class="btn btn-primary btn-form display-4">CONTACT US</button></div>
+                    </div>
+                </form><!---Formbuilder Form--->
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="cid-qTkAaeaxX5" id="footer1-2">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row content text-white">
+            <div class="col-12 col-md-3">
+                <div class="media-wrap">
+                    <a href="https://mobirise.com/">
+                        <img src="{{ asset('assets/images/swastha-nari-svg-logo.svg')}}" alt="Mobirise" title="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Address
+                </h5>
+                <p class="mbr-text">Banasthali, Kathmandu<br>Nepal</p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Contacts
+                </h5>
+                <p class="mbr-text">
+                    Email: info@swasthanaari.com
+                    <br>Phone: +977 9840259168 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<br></p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Links
+                </h5>
+                <p class="mbr-text">&nbsp;<a href="https://www.youtube.com/watch?v=D_nHGLKlaG8">Video</a><br><a href="https://www.youtube.com/watch?v=D_nHGLKlaG8"><br></a><br></p>
+            </div>
+        </div>
+        <div class="footer-lower">
+            <div class="media-container-row">
+                <div class="col-sm-12">
+                    <hr>
+                </div>
+            </div>
+            <div class="media-container-row mbr-white">
+                <div class="col-sm-6 copyright">
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        © Copyright 2020 Swastha Naari</p>
+                </div>
+                <div class="col-md-6">
+                    <div class="social-list align-right">
+                        <div class="soc-item">
+                            <a href="https://twitter.com/NaariSwastha" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-twitter socicon"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.facebook.com/swasthanaari/" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-facebook socicon"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.youtube.com/watch?v=D_nHGLKlaG8" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-youtube socicon"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.instagram.com/swasthanaari/" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-instagram socicon"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            
+                                <a href="https://github.com/soneeka/Swastha-Naari"><span class="mbr-iconfont mbr-iconfont-social mbri-github"></span></a>
+                            
+                        </div>
+                        <div class="soc-item">
+                            
+                                <a href="mailto:info@swasthanaari.com"><span class="mbr-iconfont mbr-iconfont-social socicon-mail socicon"></span></a>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="{{ asset('assets/web/assets/jquery/jquery.min.js')}}"></script>
+  <script src="{{ asset('assets/popper/popper.min.js')}}"></script>
+  <script src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5"></script>
+  <script src="https://apis.google.com/js/plusone.js"></script>
+  <script src="{{ asset('assets/facebook-plugin/facebook-script.js')}}"></script>
+  <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+  <script src="{{ asset('assets/smoothscroll/smooth-scroll.js')}}"></script>
+  <script src="{{ asset('assets/parallax/jarallax.min.js')}}"></script>
+  <script src="{{ asset('assets/dropdown/js/nav-dropdown.js')}}"></script>
+  <script src="{{ asset('assets/dropdown/js/navbar-dropdown.js')}}"></script>
+  <script src="{{ asset('assets/touchswipe/jquery.touch-swipe.min.js')}}"></script>
+  <script src="{{ asset('assets/tether/tether.min.js')}}"></script>
+  <script src="{{ asset('assets/masonry/masonry.pkgd.min.js')}}"></script>
+  <script src="{{ asset('assets/imagesloaded/imagesloaded.pkgd.min.js')}}"></script>
+  <script src="{{ asset('assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js')}}"></script>
+  <script src="{{ asset('assets/vimeoplayer/jquery.mb.vimeo_player.js')}}"></script>
+  <script src="{{ asset('assets/theme/js/script.js')}}"></script>
+  <script src="{{ asset('assets/slidervideo/script.js')}}"></script>
+  <script src="{{ asset('assets/gallery/player.min.js')}}"></script>
+  <script src="{{ asset('assets/gallery/script.js')}}"></script>
+  <script src="{{ asset('assets/formoid/formoid.min.js')}}"></script>
+  
+  
+</body>
+</html>
